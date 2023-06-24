2566dfef775fa9f3a39671b613e78b897d9e2af2
iisyos <kosuke1216a@yahoo.co.jp>
2023-06-24 16:52:53 +0900

    *feat: add one-shot-task

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,34 +1,92 @@
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = var.cluster_name
+  name               = var.cluster_name
   capacity_providers = [aws_ecs_capacity_provider.provider.name]
 }
 
 resource "aws_ecs_task_definition" "task_definition" {
-  family = "nginx"
-  network_mode = "bridge"
+  family = "one-shot-task"
+
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  cpu                = "256"
+  memory             = "512"
   container_definitions = jsonencode([
     {
-      essential   = true
-      memory      = 256
-      name        = "nginx"
-      cpu         = 1
-      image       = "nginx"
-      environment = []
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
+      "name" : "node",
+      "image" : "node:latest",
+      "essential" : true,
+      "command" : [
+        "bash",
+        "-c",
+        "node -e \"console.log(new Date().toISOString())\""
+      ],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-group" : "/ecs/one-shot-task",
+          "awslogs-region" : "ap-northeast-1",
+          "awslogs-stream-prefix" : "ecs"
         }
-      ]
+      }
     }
   ])
 }
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name = "/ecs/one-shot-task"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-one-shot-task"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_logs_policy" {
+  name = "ECSCWLogsPolicy-one-shot-task"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 
 resource "aws_ecs_service" "worker" {
   name            = var.app_name
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count   = 1
 }
 
 resource "aws_ecs_capacity_provider" "provider" {
